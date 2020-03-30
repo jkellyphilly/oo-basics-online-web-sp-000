8f3b8b55e173c629384310260ae2a4319cd3dc6a
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-30 14:10:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,10 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :color
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
   end
 
   def size
